741480c9623a35871407bd155d27a5f7bd5b0bb9
Abolfazl <abolfazlnouri17@gmail.com>
2025-03-18 12:52:51 +0330

    complete tabview

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class App(customtkinter.CTk):
         self.sidebar.grid(row=0, column=0, rowspan=4, sticky='nsew')
 
         ######## text box
-        self.textbox = customtkinter.CTkTextbox(self, wrap='word', font=customtkinter.CTkFont(size=18))
+        self.textbox = customtkinter.CTkTextbox(self, wrap='word', font=customtkinter.CTkFont(size=16), padx=5, pady=5)
         self.textbox.grid(row=0, column=1, sticky='nsew', padx=15, pady=20)
 
         self.file1 = open("MyFile1.txt", "r+")
--- tabview.py
+++ tabview.py
@@ -4,3 +4,19 @@ class MyTab(customtkinter.CTkTabview):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
+
+
+        self.add('tab1')
+        self.add('tab2')
+        self.add('tab3')
+
+        self.tab('tab1').grid_columnconfigure(0, weight=1)
+
+        self.option = customtkinter.CTkOptionMenu(self.tab('tab1'), values=['a', 'b', 'c'])
+        self.option.grid(row=0, column=0, sticky='ew', padx=55, pady=(15, 0))
+
+        self.combo = customtkinter.CTkComboBox(self.tab('tab1'), values=['a', 'b', 'c'])
+        self.combo.grid(row=1, column=0, sticky='ew', padx=55, pady=(15, 0))
+
+        self.btn = customtkinter.CTkButton(self.tab('tab1'), text='OK')
+        self.btn.grid(row=2, column=0, sticky='ew', padx=55, pady=(15, 0))
